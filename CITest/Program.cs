e8a6853fdb3fbf2ddb665749bb6d08c79e8170e2
Hassan Taherian <hassantsf.dev@gmail.com>
2024-02-25 19:17:39 +0330

    style: comment

--- CITest/Program.cs
+++ CITest/Program.cs
@@ -1,2 +1,7 @@
+<<<<<<< Updated upstream
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+=======
+﻿Console.WriteLine("Hello, World!");
+// Helo
+>>>>>>> Stashed changes
